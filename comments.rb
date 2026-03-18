360227c214810565b30d00b42ff90baba1c19f25
wfbraga <wfbraga@hotmail.com>
2026-03-18 05:38:38 -0300

    Add tip: multiline comments

---
+++ comments.rb
@@ -0,0 +1,9 @@
+# Tip: Use `=begin` / `=end` for block comments (they must start in column 1 and are mainly for RDoc).
+# Alternatively prefix each line with `#`, or wrap in `if false ... end` to temporarily disable a block.
+# A heredoc assigned to a variable (or `nil`) can also serve as a block comment when needed.
+# The code below adds 2 numbers together
+# Another comment right here
+# Another comment down below
+puts 1 + 1
+puts 2 + 2 # This will still work
+puts 3 + 3
