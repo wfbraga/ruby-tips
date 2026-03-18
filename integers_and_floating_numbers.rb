a2c9db326d9bf9edece8f7dac653afb15fcbf9d9
wfbraga <wfbraga@hotmail.com>
2026-03-18 18:59:14 -0300

    Update integers_and_floating_numbers.rb

--- integers_and_floating_numbers.rb
+++ integers_and_floating_numbers.rb
@@ -1,6 +1,9 @@
+# Both integers and floats can participate in arithmetic operations,
+# but operations on floats may introduce precision issues due to how they are represented in memory. 
+# For example, dividing two integers may yield a float (e.g., 5 / 2 produces 2.0).
 puts 100
 puts 0
-puts -837
+puts (-837)
 puts 1_000
 puts 9_999_999_999_999_999
 
