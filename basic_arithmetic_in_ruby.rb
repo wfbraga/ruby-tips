9c5c0c55aa34688928e8580407124e43b2e09d7e
wfbraga <wfbraga@hotmail.com>
2026-03-18 18:59:11 -0300

    Update basic_arithmetic_in_ruby.rb

--- basic_arithmetic_in_ruby.rb
+++ basic_arithmetic_in_ruby.rb
@@ -9,13 +9,13 @@ puts 3 * 4 * 5
 puts (2 + 3) * 5
 
 puts 10 / 5
-puts 12 / 5
+puts 12 / 5 # ruby performs integer division when both operands are integers, so this will output 2 instead of 2.4
 
-puts 12.0 / 5
-puts 12 / 5.0
-puts 12.0 / 5.0
+puts 12.0 / 5 # By making one of the operands a float (12.0), Ruby performs floating-point division, yielding 2.4 instead of 2.
+puts 12 / 5.0 # Similarly, making the second operand a float (5.0) also results in floating-point division, yielding 2.4.
+puts 12.0 / 5.0 # If both operands are floats, Ruby performs floating-point division, yielding 2.4.
 
-puts 0.5 / 2
+puts 0.5 / 2 # This will output 0.25, demonstrating that dividing a float by an integer results in a float.
 
 puts 5 % 2
 puts 7 % 2
